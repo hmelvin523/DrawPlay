3a74d957eb7c332a344c5466e2b16ba1cc68a92d
Kolton Ponte <koltonponte@mines.edu>
2017-10-03 12:00:22 -0600

    Larger Eyes

--- src/Cat.java
+++ src/Cat.java
@@ -8,11 +8,11 @@ public class Cat {
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	private static final int EYE_X = HEAD_DIMENSION/18;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 10;
+	private static final int EYE_HEIGHT = 40;
+	private static final int EYE_WIDTH = 40;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
